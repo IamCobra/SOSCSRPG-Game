5f93dcd86ccb735d2e2a6718516511a359fa616b
Ikhvan <ikhvan95@gmail.com>
2024-03-14 15:20:31 +0100

    Lavet Worldfactory og World generelt for at lave logikken på hvordan verden skal hænge sammen.

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -10,6 +10,8 @@ namespace Engine.ViewModels
     {
         public Player CurrentPlayer { get; set; }
         public Location CurrentLocation { get; set; }
+
+        public World CurrentWorld { get; set; }
         public GameSession()
         {
             CurrentPlayer = new Player();
@@ -19,12 +21,13 @@ namespace Engine.ViewModels
             CurrentPlayer.Gold = 1000000;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
-
             CurrentLocation = new Location();
             CurrentLocation.Name = "Home";
             CurrentLocation.XCoordinate = 0;
             CurrentLocation.YCoordinate = -1;
             CurrentLocation.Description = "This is your house";
+            CurrentLocation.ImageName = "pack://application:,,,/Engine;component/Images/Locations/Home.png";
+
         }
     }
 }
